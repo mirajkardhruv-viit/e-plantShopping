425629131cce2444f137a2941356505f14990f75
Dhruv <dhruv.22111324@viit.ac.in>
2024-10-25 13:51:34 -0400

    Made changes to ProductList.jsx-Task1

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -242,9 +243,16 @@ const handlePlantsClick = (e) => {
     setShowCart(false); // Hide the cart when navigating to About Us
 };
 
-   const handleContinueShopping = (e) => {
+const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
+  };
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
   };
     return (
         <div>
@@ -268,7 +276,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+						<div key={index}>
+							<h1><div>{category.category}</div></h1>
+							<div className='product-list'>
+								{category.plants.map((plant, plantIndex) => (
+									<div className='product-card' key={plantIndex}>
+										<img className='product-image' src={plant.image} alt={plant.name} />
+										<div className='product-title'>{plant.name}</div>
+										<div className='product-description'>{plant.description}</div>
+										<div className='product-cost'>{plant.cost}</div>
+										<button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+									</div>
+								))}
+							</div>
+						</div>
+					))}
 
         </div>
  ) :  (
